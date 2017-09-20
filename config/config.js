17ce80228479c5b13b27896d8d56b26e3f568329
Essau Ramirez <sauramirez@users.noreply.github.com>
2017-09-20 16:37:25 -0500

    Added sequelize configuration (#2)
    * Added sequelize configuration
    
    * Remove unused dbConfig

--- config/config.js
+++ config/config.js
@@ -5,8 +5,9 @@ const Getenv = require('getenv2');
 exports.port = Getenv('ACOPIO_API_PORT', 9999);
 
 exports.mysql = {
-  database: Getenv('ACOPIO_API_MYSQL_DATABASE'),
-  host: Getenv('ACOPIO_API_MYSQL_HOST'),
-  password: Getenv('ACOPIO_API_MYSQL_PASSWORD'),
-  user: Getenv('ACOPIO_API_MYSQL_USER')
+  database: Getenv('ACOPIO_API_MYSQL_DATABASE', 'acopiodb'),
+  host: Getenv('ACOPIO_API_MYSQL_HOST', 'localhost'),
+  port: Getenv('ACOPIO_API_MYSQL_HOST', 3306),
+  password: Getenv('ACOPIO_API_MYSQL_PASSWORD', ''),
+  user: Getenv('ACOPIO_API_MYSQL_USER', 'acopio')
 };
