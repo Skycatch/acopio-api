08e5df93cf97d10c96f1cc26c0d80c85c1cd9827
Essau Ramirez <sauramirez@users.noreply.github.com>
2017-09-20 18:10:54 -0500

    Added authentication support (#4)
    * Added authentication support
    
    * Removed invalid comments

--- config/config.js
+++ config/config.js
@@ -4,10 +4,16 @@ const Getenv = require('getenv2');
 
 exports.port = Getenv('ACOPIO_API_PORT', 9999);
 
+exports.auth0 = {
+  clientId: Getenv('AUTH0_CLIENT'),
+  clientSecret: Getenv('AUTH0_CLIENT_SECRET'),
+  domain: Getenv('AUTH0_DOMAIN', 'acopio-api.auth0.com')
+};
+
 exports.mysql = {
   database: Getenv('ACOPIO_API_MYSQL_DATABASE', 'acopiodb'),
   host: Getenv('ACOPIO_API_MYSQL_HOST', 'localhost'),
-  port: Getenv('ACOPIO_API_MYSQL_HOST', 3306),
+  port: Getenv('ACOPIO_API_MYSQL_PORT', 3306),
   password: Getenv('ACOPIO_API_MYSQL_PASSWORD', ''),
   user: Getenv('ACOPIO_API_MYSQL_USER', 'acopio')
 };
