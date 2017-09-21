6283dc902b1e53ecd3718122570edcd945281b4e
Essau <saumotions@gmail.com>
2017-09-20 21:20:06 -0500

    Addressing comments

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const Getenv = require('getenv2');
 exports.port = Getenv('ACOPIO_API_PORT', 9999);
 
 exports.auth0 = {
-  audience: Getenv('AUTH0_AUDIENCE', 'https://acopio-api.skycatch.net'),
+  audience: Getenv('ACOPIO_API_AUTH0_AUDIENCE', 'https://acopio-api.skycatch.net'),
   clientId: Getenv('ACOPIO_API_AUTH0_CLIENT'),
   clientSecret: Getenv('ACOPIO_API_AUTH0_CLIENT_SECRET'),
   domain: Getenv('ACOPIO_API_AUTH0_DOMAIN', 'acopio-api.auth0.com'),
