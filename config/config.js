1267fd1b4c80f397e538506b95a4a81dd9580cae
Essau <saumotions@gmail.com>
2017-09-20 21:09:31 -0500

    Added working post requests to create new centro de acopio

--- config/config.js
+++ config/config.js
@@ -5,9 +5,11 @@ const Getenv = require('getenv2');
 exports.port = Getenv('ACOPIO_API_PORT', 9999);
 
 exports.auth0 = {
+  audience: Getenv('AUTH0_AUDIENCE', 'https://acopio-api.skycatch.net'),
   clientId: Getenv('ACOPIO_API_AUTH0_CLIENT'),
   clientSecret: Getenv('ACOPIO_API_AUTH0_CLIENT_SECRET'),
-  domain: Getenv('ACOPIO_API_AUTH0_DOMAIN', 'acopio-api.auth0.com')
+  domain: Getenv('ACOPIO_API_AUTH0_DOMAIN', 'acopio-api.auth0.com'),
+  jwksUri: Getenv('ACOPIO_API_AUTH0_DOMAIN', 'https://acopio-api.auth0.com/.well-known/jwks.json')
 };
 
 exports.mysql = {
